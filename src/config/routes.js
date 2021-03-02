daf75ab22eb25a2f88a00f73e1d1ddbb40f6541a
biggersmatt <biggersmatt73@gmail.com>
2021-03-02 15:11:19 -0800

    Adds NewEndcapPage

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,9 +1,11 @@
 import { Switch, Route } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
+import NewEndcapPage from '../pages/NewEndcapPage';
 
 const routes = (
   <Switch>
     <Route exact path='/' component={HomePage} />
+    <Route exact path='/new' component={NewEndcapPage} />
   </Switch>
 );
 
---
+++ src/pages/NewEndcapPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NewEndcapPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>New Endcap Page</h1>
+      </div>
+    )
+  }
+}
+
+export default NewEndcapPage;
