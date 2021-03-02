429aabe2ad6a374fc03e157d5c48e81292b926a1
biggersmatt <biggersmatt73@gmail.com>
2021-03-02 15:42:47 -0800

    Can create New Endcaps

--- src/pages/NewEndcapPage.js
+++ src/pages/NewEndcapPage.js
@@ -12,12 +12,22 @@ class NewEndcapPage extends React.Component {
     })
   }
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    fetch('http://localhost:4000/api/endcaps', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(this.state),
+    })
+  }
+
   render() {
     return (
       <div>
         <h1>Add New Endcap</h1>
-
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div>
             <label htmlFor="title">Title</label><br />
             <input type="text" id="title" name="title" value={this.state.title} onChange={this.handleChange} />
@@ -26,8 +36,8 @@ class NewEndcapPage extends React.Component {
             <label htmlFor="itemOne">Item One</label><br />
             <input type="text" id="itemOne" name="itemOne" value={this.state.itemOne} onChange={this.handleChange} />
           </div>
+          <button type="submit">Add Endcap</button>
         </form>
-
       </div>
     )
   }
