e11e55c84a7d361f7df34ff103d61ad8f0b9f3bb
biggersmatt <biggersmatt73@gmail.com>
2021-03-02 15:22:49 -0800

    Adds form and handleChange to NewEndcap

--- src/pages/NewEndcapPage.js
+++ src/pages/NewEndcapPage.js
@@ -1,10 +1,33 @@
 import React from 'react';
 
 class NewEndcapPage extends React.Component {
+  state = {
+    title: '',
+    itemOne: '',
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.id]: event.target.value,
+    })
+  }
+
   render() {
     return (
       <div>
-        <h1>New Endcap Page</h1>
+        <h1>Add New Endcap</h1>
+
+        <form>
+          <div>
+            <label htmlFor="title">Title</label><br />
+            <input type="text" id="title" name="title" value={this.state.title} onChange={this.handleChange} />
+          </div>
+          <div>
+            <label htmlFor="itemOne">Item One</label><br />
+            <input type="text" id="itemOne" name="itemOne" value={this.state.itemOne} onChange={this.handleChange} />
+          </div>
+        </form>
+
       </div>
     )
   }
