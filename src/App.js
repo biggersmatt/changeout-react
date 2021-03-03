c33231e4d5af45f6f021ccce963a14aae27b3204
biggersmatt <biggersmatt73@gmail.com>
2021-03-03 10:11:29 -0800

    Adds styles to header of homepage

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div>
       <nav>
         <img src="https://images.unsplash.com/photo-1563861826100-9cb868fdbe1c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="Logo"/>
-        <h1>Change Out</h1>
+        <h1>Welcome</h1>
         <ul className="nav-items">
           <li>
             <Link to='/'>Home</Link>{' '}
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -38,6 +38,42 @@ class HomePage extends React.Component {
   render() {
     return (
       <div>
+        <header>
+          <div className="homepage-new-endcap">
+            <button className="homepage-new-btn">+</button>
+            <h3>New Endcap</h3>
+          </div>
+          <h1 className="homepage-title">Change Out</h1>
+          <div className="homepage-promo">
+            <ul>
+              <li className="homepage-promo-title">Promo</li>
+              <li className="homepage-promo-selection">
+                <p>Month</p>
+                <select>
+                  <option value="1">January</option>
+                  <option value="2">February</option>
+                  <option value="3">March</option>
+                  <option value="4">April</option>
+                  <option value="5">May</option>
+                  <option value="6">June</option>
+                  <option value="7">July</option>
+                  <option value="8">August</option>
+                  <option value="9">September</option>
+                  <option value="10">October</option>
+                  <option value="11">November</option>
+                  <option value="12">December</option>
+                </select>
+              </li>
+              <li className="homepage-promo-selection">
+                <p>Period</p>
+                <select>
+                  <option value="1">A</option>
+                  <option value="2">B</option>
+                </select>
+              </li>
+            </ul>
+          </div>
+        </header>
         <EndcapsList endcaps={this.state.endcaps} deleteEndcap={this.handleDeleteEndcap}/>
       </div>
     )
