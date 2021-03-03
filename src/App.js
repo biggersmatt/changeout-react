5f6c4d004581a3ca7ab19f446e05efe774bf478d
biggersmatt <biggersmatt73@gmail.com>
2021-03-03 11:10:05 -0800

    Footer stays at the bottom of the page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="test">
       <nav>
         <img src="https://images.unsplash.com/photo-1563861826100-9cb868fdbe1c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="Logo"/>
         <h1>Welcome</h1>
