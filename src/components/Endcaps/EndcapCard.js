ec767deb69ff06af713e564cda297fb142d984a1
biggersmatt <biggersmatt73@gmail.com>
2021-03-03 10:26:34 -0800

    Creates and connects endcap css sheet

--- src/components/Endcaps/EndcapCard.js
+++ src/components/Endcaps/EndcapCard.js
@@ -1,16 +1,14 @@
 import { Link } from 'react-router-dom';
+require('./Endcap.css')
 
 const EndcapCard = (props) => {
   return (
-    <>
-      <div>
-        <h3>{props.endcap.title}</h3>
-        <p>{props.endcap.itemOne}</p>
-      </div>
+    <div className="endcap-card">
+      <h3>{props.endcap.title}</h3>
+      <p>{props.endcap.itemOne}</p>
       <button onClick={() => props.deleteEndcap(props.endcap._id)}>Delete</button>{' '}
       <Link to={`/edit/${props.endcap._id}`}><button>Edit</button></Link>
-      <hr />
-    </>
+    </div>
   )
 }
 
