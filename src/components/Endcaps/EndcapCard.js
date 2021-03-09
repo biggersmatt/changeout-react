7e2807b0cac56dab79f583d9484b3bfd1682c5dd
biggersmatt <biggersmatt73@gmail.com>
2021-03-09 14:55:36 -0800

    Refactors Endcap Card

--- src/components/Endcaps/EndcapCard.js
+++ src/components/Endcaps/EndcapCard.js
@@ -4,39 +4,31 @@ import { Draggable } from 'react-beautiful-dnd';
 require('./Endcap.css')
 
 class EndcapCard extends React.Component {
-  state = {
-    title: this.props.endcap.title,
-    itemOne: this.props.endcap.itemOne,
-    itemTwo: this.props.endcap.itemTwo,
-    itemThree: this.props.endcap.itemThree,
-    itemFour: this.props.endcap.itemFour,
-    itemFive: this.props.endcap.itemFive,
-    change: this.props.endcap.change,
-    flankA: '',
-    flankB: '',
-  }
+  // state = {
 
-  componentDidMount() {
-    fetch('http://localhost:4000/api/flanks')
-      .then((response) => response.json())
-      .then((jsonData) => {
-        const allFlanks = jsonData.allFlanks;
-        let flankA = '';
-        let flankB = '';
-        allFlanks.forEach((flank) => {
-          if(flank._id === this.props.endcap.flanks[0]) {
-            flankA = flank;
-          }
-          if(flank._id === this.props.endcap.flanks[1]) {
-            flankB = flank;
-          }
-        })
-        this.setState({
-          flankA: flankA,
-          flankB: flankB,
-        })
-      })
-  }
+  // }
+
+  // componentDidMount() {
+  //   fetch('http://localhost:4000/api/flanks')
+  //     .then((response) => response.json())
+  //     .then((jsonData) => {
+  //       const allFlanks = jsonData.allFlanks;
+  //       let flankA = '';
+  //       let flankB = '';
+  //       allFlanks.forEach((flank) => {
+  //         if(flank._id === this.props.endcap.flanks[0]) {
+  //           flankA = flank;
+  //         }
+  //         if(flank._id === this.props.endcap.flanks[1]) {
+  //           flankB = flank;
+  //         }
+  //       })
+  //       this.setState({
+  //         flankA: flankA,
+  //         flankB: flankB,
+  //       })
+  //     })
+  // }
 
   handleToggleClass = (event) => {
     event.preventDefault();
@@ -65,16 +57,16 @@ class EndcapCard extends React.Component {
           >
             {/* Flank A */}
             <div className="flank">
-              <h4>{this.state.flankA.title}</h4>
-              <p>{this.state.flankA.itemOne}</p>
-              <p>{this.state.flankA.itemTwo}</p>
-              <p>{this.state.flankA.itemThree}</p>
-              <p>{this.state.flankA.itemFour}</p>
-              <p>{this.state.flankA.itemFive}</p>
+              <h4>{this.props.endcap.flanks[0] ? this.props.endcap.flanks[0].title : ''}</h4>
+              <p>{this.props.endcap.flanks[0] ? this.props.endcap.flanks[0].itemOne : ''}</p>
+              <p>{this.props.endcap.flanks[0] ? this.props.endcap.flanks[0].itemTwo : ''}</p>
+              <p>{this.props.endcap.flanks[0] ? this.props.endcap.flanks[0].itemThree : ''}</p>
+              <p>{this.props.endcap.flanks[0] ? this.props.endcap.flanks[0].itemFour : ''}</p>
+              <p>{this.props.endcap.flanks[0] ? this.props.endcap.flanks[0].itemFive : ''}</p>
               <div>
-                <Link to={`/edit/${this.props.endcap._id}/flank/${this.state.flankA._id}`}>
+                {/* <Link to={`/edit/${this.props.endcap._id}/flank/${this.state.flankA._id}`}>
                   <button>Edit</button>
-                </Link>
+                </Link> */}
                 <div>
                   <h4>Change</h4>
                   <input type="checkbox"/>
@@ -82,7 +74,9 @@ class EndcapCard extends React.Component {
               </div>
             </div>
             {/* Endcap */}
-            <div className="endcap-card" id={this.state.change ? 'yellow': null}>
+            <div className="endcap-card" 
+            // id={this.props.endcap.change && 'yellow'}
+            >
               <h3>{this.props.endcap.title}</h3>
               <p>{this.props.endcap.itemOne}</p>
               <p>{this.props.endcap.itemTwo}</p>
@@ -102,16 +96,16 @@ class EndcapCard extends React.Component {
             </div>
             {/* Flank B */}
             <div className="flank">
-              <h4>{this.state.flankB.title}</h4>
-              <p>{this.state.flankB.itemOne}</p>
-              <p>{this.state.flankB.itemTwo}</p>
-              <p>{this.state.flankB.itemThree}</p>
-              <p>{this.state.flankB.itemFour}</p>
-              <p>{this.state.flankB.itemFive}</p>
+            <h4>{this.props.endcap.flanks[1] ? this.props.endcap.flanks[1].title : ''}</h4>
+              <p>{this.props.endcap.flanks[1] ? this.props.endcap.flanks[1].itemOne : ''}</p>
+              <p>{this.props.endcap.flanks[1] ? this.props.endcap.flanks[1].itemTwo : ''}</p>
+              <p>{this.props.endcap.flanks[1] ? this.props.endcap.flanks[1].itemThree : ''}</p>
+              <p>{this.props.endcap.flanks[1] ? this.props.endcap.flanks[1].itemFour : ''}</p>
+              <p>{this.props.endcap.flanks[1] ? this.props.endcap.flanks[1].itemFive : ''}</p>
               <div>
-                <Link to={`/edit/${this.props.endcap._id}/flank/${this.state.flankB._id}`}>
+                {/* <Link to={`/edit/${this.props.endcap._id}/flank/${this.state.flankB._id}`}>
                   <button>Edit</button>
-                </Link>
+                </Link> */}
                 <div>
                   <h4>Change</h4>
                   <input type="checkbox"/>
