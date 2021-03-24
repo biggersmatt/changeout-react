04d2a67f460a3958258e1429cb0eaf02a8bd1ca9
biggersmatt <biggersmatt73@gmail.com>
2021-03-24 11:27:07 -0700

    Creates navbar css page

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,14 @@
 import { Link } from 'react-router-dom';
+require('./Navbar.css');
 
 const Navbar = () => {
   return (
     <nav>
       <img src="https://images.unsplash.com/photo-1563861826100-9cb868fdbe1c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="Logo"/>
-      <h1>Welcome</h1>
+      <h1>Welcome username</h1>
       <ul className="nav-items">
         <li>
           <Link to='/'>Home</Link>{' '}
-        </li>
-        <li>
-          <Link to='/new'>New Endcap</Link>
         </li>
       </ul>
     </nav>
