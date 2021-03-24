49dced55e31933a798751415a3462ea3a04584e5
biggersmatt <biggersmatt73@gmail.com>
2021-03-24 11:49:09 -0700

    Refactors and styles Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,13 +4,8 @@ require('./Navbar.css');
 const Navbar = () => {
   return (
     <nav>
-      <img src="https://images.unsplash.com/photo-1563861826100-9cb868fdbe1c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="Logo"/>
-      <h1>Welcome username</h1>
-      <ul className="nav-items">
-        <li>
-          <Link to='/'>Home</Link>{' '}
-        </li>
-      </ul>
+      <h1 className="navbar-greeting">Welcome Username</h1>
+      <Link to='/'><span className="navbar-link">Home</span></Link>{' '}
     </nav>
   )
 }
