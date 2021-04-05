db1d7ea9aca8302dc0a6871ec0ea0f2799f409d4
biggersmatt <biggersmatt73@gmail.com>
2021-04-05 11:11:23 -0700

    Adds styles to LoginPage

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -15,10 +15,13 @@ const LoginPage = (props) => {
   return (
     <div className="login-container">
       <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
+        <h1>Change Out</h1>
+        <h2 className="login-form-signup">Sign In</h2>
         <div className="login-form-section">
           <label className="login-form-label" htmlFor="company">Company</label>
           <input 
-            className="login-form-input" 
+            className="login-form-input"
+            placeholder="Your company"
             type="text" 
             name="company" 
             id="company" 
@@ -28,13 +31,15 @@ const LoginPage = (props) => {
         <div className="login-form-section">
           <label className="login-form-label" htmlFor="password">Password</label>
           <input 
-            className="login-form-input" 
+            className="login-form-input"
+            placeholder="Your password"
             type="password" 
             name="password" 
             id="password" 
             ref={register({ required: true })} 
           />
         </div>
+        <h4>Don't have an account? <a href="www.google.com"><span>Sign up</span></a>.</h4>
         <button className="login-submit-btn" type="submit">Login</button>
       </form>
     </div>
