968cfd9c2752b0487528574aee103f33b441b53c
biggersmatt <biggersmatt73@gmail.com>
2021-04-05 13:56:40 -0700

    Adds styles for responsiveness to Navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,8 +5,10 @@ const Navbar = (props) => {
   return (
     <nav>
       <h1 className="navbar-greeting">Welcome Username</h1>
-      <Link to='/' className="navbar-link">Home</Link>
-      <Link to='/login' onClick={() => props.logout()} className="navbar-link">Log Out</Link>
+      <div className="navbar-links">
+        <Link to='/' className="navbar-link">Home</Link>
+        <Link to='/login' onClick={() => props.logout()} className="navbar-link">Sign out</Link>
+      </div>
     </nav>
   )
 }
