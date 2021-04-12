4722bd6c13ba0d87255cdb993ffbf2ee04796e46
biggersmatt <biggersmatt73@gmail.com>
2021-04-12 12:27:20 -0700

    Adds ternary to EditEndcapPage

--- src/pages/Endcaps/EditEndcap/EditEndcapPage.js
+++ src/pages/Endcaps/EditEndcap/EditEndcapPage.js
@@ -10,6 +10,8 @@ class EditEndcapPage extends React.Component {
     itemThree: '',
     itemFour: '',
     itemFive: '',
+    flankA: '',
+    flankB: '',
   }
 
   componentDidMount() {
@@ -80,7 +82,7 @@ class EditEndcapPage extends React.Component {
     return (
       <div className="edit-endcap-wrapper">
         <h1 className="edit-endcap-title">Edit {this.state.title}</h1>
-        <div className="edit-endcap-header-btns">
+        <div id={`${(this.props.endcaps.length === 1 && (this.state.flankA && this.state.flankB)) ? 'hidden' : null}`} className="edit-endcap-header-btns">
           <div className="edit-endcap-btn-wrapper" id={this.props.endcaps.length === 1 ? 'hidden' : null}>
             <Link to="/">
               <i 
@@ -91,7 +93,7 @@ class EditEndcapPage extends React.Component {
             </Link>
             <h4 className="edit-endcap-btn-title">Delete</h4>
           </div>
-          <div className="edit-endcap-btn-wrapper">
+          <div id={`${(this.state.flankA && this.state.flankB) ? 'hidden' : null}`} className="edit-endcap-btn-wrapper">
             <Link to={`/edit/${this.props.match.params.id}/flank/new`}>
               <i 
                 className="far fa-plus-square edit-endcap-flank-btn"
