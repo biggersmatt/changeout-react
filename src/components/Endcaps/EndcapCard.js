09af1a7c8480aff4fb34f435b6aed8ac5ca58275
biggersmatt <biggersmatt73@gmail.com>
2021-04-19 13:28:09 -0700

    Clears comments from EndcapList and EndcapCard

--- src/components/Endcaps/EndcapCard.js
+++ src/components/Endcaps/EndcapCard.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-// import { Draggable } from "react-beautiful-dnd";
 import FlankA from "../Flanks/FlankA";
 import FlankB from "../Flanks/FlankB";
 require("./EndcapCard.css")
@@ -33,61 +32,41 @@ function EndcapCard(props) {
     .catch((err) => console.log(err));
   }
 
-
-
-  console.log("EndcapCard");
   return (
-    // <Draggable draggableId={props.endcap._id} index={props.index}>
-      // {(provided, snapshot) => (
-        <div className="endcap-flank-container"
-          // {...provided.draggableProps}
-          // {...provided.dragHandleProps}
-          // ref={provided.innerRef}
-        >
-          <FlankA 
-            endcap={props.endcap} 
-            // flankA={props.flankA} 
-            // handleToggleFlank={props.handleToggleFlank}
-          />
-          <div 
-            className={`endcap-card shadow ${change && "yellow"}`} 
-            id={
-              (flankA.title && flankB.title ? "nothing" : null) ||
-              (flankA.title ? "endcap-flank-a": "") || 
-              (flankB.title ? "endcap-flank-b" : "") 
-            }
-          >
-            <h3 className="endcap-card-title">{props.endcap.title}</h3>
-            <p className="endcap-card-item">{props.endcap.itemOne}</p>
-            <p className="endcap-card-item">{props.endcap.itemTwo}</p>
-            <p className="endcap-card-item">{props.endcap.itemThree}</p>
-            <p className="endcap-card-item">{props.endcap.itemFour}</p>
-            <p className="endcap-card-item">{props.endcap.itemFive}</p>
-            <p className="endcap-card-item">{change}</p>
-            <div className="endcap-card-btns">
-              <Link to={`/edit/${props.endcap._id}`}>
-                <p className={change ? "endcap-edit-completed" : "endcap-edit-change"}>Edit Info</p>
-              </Link>
-              <div>
-                <p 
-                  className={change ? "endcap-completed" : "endcap-change"}
-                  // onClick={() => props.handleToggleEndcap(props.endcap.change, props.endcap._id)}
-                  onClick={() => handleToggleEndcap(props.endcap)}
-                >
-                  {change ? "Complete" : "Change"}
-                </p>
-              </div>
-            </div>
+    <div className="endcap-flank-container">
+      <FlankA endcap={props.endcap} />
+      <div 
+        className={`endcap-card shadow ${change && "yellow"}`} 
+        id={
+          (flankA.title && flankB.title ? "nothing" : null) ||
+          (flankA.title ? "endcap-flank-a": "") || 
+          (flankB.title ? "endcap-flank-b" : "") 
+        }
+      >
+        <h3 className="endcap-card-title">{props.endcap.title}</h3>
+        <p className="endcap-card-item">{props.endcap.itemOne}</p>
+        <p className="endcap-card-item">{props.endcap.itemTwo}</p>
+        <p className="endcap-card-item">{props.endcap.itemThree}</p>
+        <p className="endcap-card-item">{props.endcap.itemFour}</p>
+        <p className="endcap-card-item">{props.endcap.itemFive}</p>
+        <p className="endcap-card-item">{change}</p>
+        <div className="endcap-card-btns">
+          <Link to={`/edit/${props.endcap._id}`}>
+            <p className={change ? "endcap-edit-completed" : "endcap-edit-change"}>Edit Info</p>
+          </Link>
+          <div>
+            <p 
+              className={change ? "endcap-completed" : "endcap-change"}
+              onClick={() => handleToggleEndcap(props.endcap)}
+            >
+              {change ? "Complete" : "Change"}
+            </p>
           </div>
-          <FlankB 
-            endcap={props.endcap} 
-            // flankB={props.flankB} 
-            // handleToggleFlank={props.handleToggleFlank}
-          />
         </div>
-      // )}
-    // </Draggable>
-    )
+      </div>
+      <FlankB endcap={props.endcap} />
+    </div>
+  )
 }
 
 export default EndcapCard;
--- src/components/Endcaps/EndcapsList.js
+++ src/components/Endcaps/EndcapsList.js
@@ -1,32 +1,20 @@
-// import { Droppable } from "react-beautiful-dnd";
 import EndcapCard from "../Endcaps/EndcapCard";
 require("./EndcapCard.css");
 
 const EndcapsList = (props) => {
-  console.log("EndcapList");
-    return (
-      // <Droppable droppableId={props.column.id}>
-        // {(provided, snapshot) => (
-        <div
-          // ref={provided.innerRef}
-          // {...provided.droppableProps}
-        >
-          {props.endcaps.map((endcap, index) => {
-            return <EndcapCard 
-                      handleToggleEndcap={props.handleToggleEndcap}
-                      handleToggleFlank={props.handleToggleFlank}
-                      key={endcap._id} 
-                      index={index} 
-                      endcap={endcap}
-                      // flankA={endcap.flankA}
-                      // flankB={endcap.flankB}
-                    />
-          })}
-          {/* {provided.placeholder} */}
-        </div>
-        // )}
-      // </Droppable>
-    )
+  return (
+    <div>
+      {props.endcaps.map((endcap, index) => {
+        return <EndcapCard 
+                  handleToggleEndcap={props.handleToggleEndcap}
+                  handleToggleFlank={props.handleToggleFlank}
+                  key={endcap._id} 
+                  index={index} 
+                  endcap={endcap}
+                />
+      })}
+    </div>
+  )
 };
 
 export default EndcapsList;
